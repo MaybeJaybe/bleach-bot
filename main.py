34c9186e4f0b06bbc8f0fd77d162f2d1111484cf
Ryan Lee <rjlguitar@gmail.com>
2021-07-13 12:00:38 -0700

    added cat picture command, change $ to !

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ def get_quote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
 
+# function for random cat picture
+def getCatPicture():
+    catPicture = requests.get('http://thecatapi.com/api/images/get.php')
+    if catPicture.status_code == 200:
+        catPicture = catPicture.url
+        return catPicture
+
+    else:
+        return 'Error 404. Website may be down.'
+
 # prints to console that bot is active
 @client.event
 async def on_ready():
@@ -23,13 +33,17 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
     
     # command for inspirational quotes
-    if message.content.startswith('$inspire'):
+    if message.content.startswith('!inspire'):
         quote = get_quote()
         await message.channel.send(quote)
+
+    if message.content.startswith('!cat'):
+        catpic = getCatPicture()
+        await message.channel.send(catpic)
 
 # running the bot with its code
 client.run('ODYyNzUwNzgwMDQ1NjU2MDk0.YOc5Rw.zrWiNQb6IW9EXbYizOqMD3BcHoo')
